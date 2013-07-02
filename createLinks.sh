d379f56f51b4ee24ecfe9df72012ea355bea28b6
Alex Buloichik <alex73mail@gmail.com>
2013-07-02 08:39:01 +0300

    Initial project

---
+++ createLinks.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -x
+ln -s ../../Common/en-be dictionary/en-be
+ln -s ../../Common/glossaries glossary/common
+ln -s ../../Common.TMX/tmx/non-segmented/ tm/common
---
+++ get.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget --no-check-certificate -O source/strings.xml https://raw.github.com/AnySoftKeyboard/AnySoftKeyboard/master/res/values/strings.xml
